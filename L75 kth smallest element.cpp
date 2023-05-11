3b0249141c6f62d3e2d41a0f8ce5377f75276a2b
manujk25 <117919801+manujk25@users.noreply.github.com>
2023-05-11 20:25:58 +0530

    Create L75 kth smallest element.cpp
    lecture number 75

---
+++ L75 kth smallest element.cpp
@@ -0,0 +1,27 @@
+ //User function template for C++
+//kth smallest element using the Heap 
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        priority_queue<int> pq;
+        for(int i=0;i<k;i++){
+            pq.push(arr[i]);
+        }
+        
+        //step2:
+        for(int i = k;i<=r;i++){
+            if(arr[i] < pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        //step 3
+        int ans =pq.top();
+        return ans;
+    }
+};
