8d75b2635eb858d15a5e4f3d0b822d82e15bb647
manujk25 <117919801+manujk25@users.noreply.github.com>
2023-07-13 15:27:59 +0530

    Create L106HouseRobberyproblem.cpp

---
+++ L106HouseRobberyproblem.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+long long int solveSop(vector<int> & nums){
+    int n = nums.size();
+    long long int prev2 = 0;
+    long long int prev1 = nums[0];
+
+    for(int i=1;i<=n-1;i++){ 
+        long long int include = prev2+nums[i];
+        long long int exclude = prev1+0;
+
+        int ans = max(include,exclude);
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    return prev1;
+}
+long long int houseRobber(vector<int>& valueInHouse)
+{
+    int n = valueInHouse.size();
+
+    if(n==1){
+        return valueInHouse[0];
+    }
+    vector<int> first, second;
+
+    for(int i=0;i<n;i++){
+       if(i != n-1){
+           first.push_back(valueInHouse[i]);
+       } 
+       if( i != 0){
+           second.push_back(valueInHouse[i]);
+       }
+    }
+    return max(solveSop(first),solveSop(second));
+}
